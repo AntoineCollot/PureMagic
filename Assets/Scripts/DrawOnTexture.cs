806ed8e2fde49324eeb7eb7962691667d71c7eab
AntoineCollot <antoine.collot@live.com>
2019-05-26 18:32:55 +0200

    Added image cleaning using compute shaders

--- Assets/Scripts/DrawOnTexture.cs
+++ Assets/Scripts/DrawOnTexture.cs
@@ -48,6 +48,8 @@ public class DrawOnTexture : MonoBehaviour
         mat = worms.material;
         Texture2D paintMap = (Texture2D)mat.GetTexture("_PaintMap");
         rt = new RenderTexture(paintMap.width, paintMap.height, 32);
+        //Make it writable so the clean image compute shader can write on it
+        rt.enableRandomWrite = true;
         Graphics.Blit(paintMap, rt);
         this.currentPaintMap = paintMap;
 
@@ -99,6 +101,9 @@ public class DrawOnTexture : MonoBehaviour
         pixelCleared = deltaPixelDrawn;
 
         UpdateMap();
+
+        //Clean the image using a compute shader
+        CleanImage.CleanCompute(ref rt);
     }
 
     public List<Vector2> GetIntermediatePositions(Vector2 currentPos, Vector2 lastPos)
@@ -121,71 +126,5 @@ public class DrawOnTexture : MonoBehaviour
         positions.Add(currentPos);
 
         return positions;
-    }
-
-    [ContextMenu("Debug Clean")]
-    public void DebugClean()
-    {
-        int debug;
-        CleanImage(ref currentPaintMap, out debug);
-        UpdateMap();
-    }
-
-    //Too costly ~5 to 10 ms in profiler, 2 in real time
-    public void CleanImage(ref Texture2D image, out int whitePixelsRemaining)
-    {
-        //float time = Time.realtimeSinceStartup;
-        //int pixelCleared = 0;
-
-        whitePixelsRemaining = 0;
-        float currentPixelValue;
-        for (int y = 0; y < image.height; y++)
-        {
-            for (int x = 0; x < image.width; x++)
-            {
-                currentPixelValue = image.GetPixel(x, y).r;
-
-                //Count the white pixels at the same time
-                if(currentPixelValue==1)
-                {
-                    whitePixelsRemaining++;
-                } 
-                //Check if it's a edge pixel (grey, so not white, not black)
-                else if (currentPixelValue>0)
-                {
-                    bool foundWhite = false;
-                    //Check if any of its neighbourg is a white pixel (pixel with stuff on it)
-                    for (int yn = -1; yn <= 1; yn++)
-                    {
-                        for (int xn = -1; xn <= 1; xn++)
-                        {
-                            //ignore ourself
-                            if (xn == 0 && yn == 0)
-                                continue;
-  
-                            if(image.GetPixel(x+xn,y+yn).r >= 1)
-                            {
-                                foundWhite = true;
-                                break;
-                            }
-                        }
-
-                        if (foundWhite)
-                            break;
-                    }
-
-                    //Turn the pixel black if we didn't find any white nearby
-                    if (!foundWhite)
-                    {
-                        image.SetPixel(x, y, Color.black);
-                        //pixelCleared++;
-                    }
-
-                }
-            }
-        }
-
-        //image.Apply();
-        //print("Cleared " + pixelCleared + " pixels in "+(Time.realtimeSinceStartup - time));
     }
 }
---
+++ Assets/Scripts/PaintMap/CleanImage.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Clean the lonely pixels using a compute shader (GPU)
+/// </summary>
+public static class CleanImage
+{
+    ////Too costly ~5 to 10 ms in profiler, 2 in real time
+    //public static void Clean(ref Texture2D image, out int whitePixelsRemaining)
+    //{
+    //    //float time = Time.realtimeSinceStartup;
+    //    //int pixelCleared = 0;
+
+    //    whitePixelsRemaining = 0;
+    //    float currentPixelValue;
+    //    for (int y = 0; y < image.height; y++)
+    //    {
+    //        for (int x = 0; x < image.width; x++)
+    //        {
+    //            currentPixelValue = image.GetPixel(x, y).r;
+
+    //            //Count the white pixels at the same time
+    //            if (currentPixelValue == 1)
+    //            {
+    //                whitePixelsRemaining++;
+    //            }
+    //            //Check if it's a edge pixel (grey, so not white, not black)
+    //            else if (currentPixelValue > 0)
+    //            {
+    //                bool foundWhite = false;
+    //                //Check if any of its neighbourg is a white pixel (pixel with stuff on it)
+    //                for (int yn = -1; yn <= 1; yn++)
+    //                {
+    //                    for (int xn = -1; xn <= 1; xn++)
+    //                    {
+    //                        //ignore ourself
+    //                        if (xn == 0 && yn == 0)
+    //                            continue;
+
+    //                        if (image.GetPixel(x + xn, y + yn).r >= 1)
+    //                        {
+    //                            foundWhite = true;
+    //                            break;
+    //                        }
+    //                    }
+
+    //                    if (foundWhite)
+    //                        break;
+    //                }
+
+    //                //Turn the pixel black if we didn't find any white nearby
+    //                if (!foundWhite)
+    //                {
+    //                    image.SetPixel(x, y, Color.black);
+    //                    //pixelCleared++;
+    //                }
+
+    //            }
+    //        }
+    //    }
+
+    //    //image.Apply();
+    //    //print("Cleared " + pixelCleared + " pixels in "+(Time.realtimeSinceStartup - time));
+    //}
+
+    static ComputeShader cshader;
+    static int kernelHandle;
+
+    static CleanImage()
+    {
+        cshader = (ComputeShader)Resources.Load("Shaders/CleanImageShader");
+        kernelHandle = cshader.FindKernel("CSMain");
+    }
+
+    public static void CleanCompute(ref RenderTexture renderTexture)
+    {
+        cshader.SetTexture(kernelHandle, "Result", renderTexture);
+        cshader.Dispatch(kernelHandle, renderTexture.width / 8, renderTexture.height / 2, 1);
+    }
+}
